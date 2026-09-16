7597252c5bc9886970354639482a41e2aca635f0
Tekton <dylanuijthof07@gmail.com>
2026-09-16 21:55:48 +0200

    FIX: better error handling and minor fixes

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,23 +1,36 @@
 use std::io;
 
 fn main() {
+    println!("number 1: ");
     let mut n1_input = String::new();
-    io::stdin()
-        .read_line(&mut n1_input)
-        .expect("failed to read line");
-    let n1: f64 = n1_input.trim().parse().expect("not a valid numer");
+    io::stdin().read_line(&mut n1_input).expect("failed to read line");
+    let n1: f64 = match n1_input.trim().parse() {
+                Ok(n) => n,
+                Err(_) => {
+                    println!("input is not a valid number");
+                    std::process::exit(1);
+                }
+            };
 
+    println!("operator: ");
     let mut op_input = String::new();
     io::stdin()
         .read_line(&mut op_input)
         .expect("failed to read line");
     let op = op_input.trim();
 
+    println!("number 2: ");
     let mut n2_input = String::new();
     io::stdin()
         .read_line(&mut n2_input)
         .expect("failed to read line");
-    let n2: f64 = n2_input.trim().parse().expect("not a valid number");
+    let n2: f64 = match n2_input.trim().parse() {
+                Ok(n) => n,
+                Err(_) => {
+                    println!("input is not a valid number");
+                    std::process::exit(1);
+                }
+            };
 
     let ans: f64;
 
@@ -26,9 +39,9 @@ fn main() {
         "-" => sub(n1, n2),
         "*" => mul(n1, n2),
         "/" => div(n1, n2),
-        _ => panic!("unknown operator"),
+        _ => {println!("unknown operator"); std::process::exit(1);},
     };
-    println!("{ans}");
+    println!("answer: {ans}");
 }
 
 fn add(n1: f64, n2: f64) -> f64 {
@@ -43,9 +56,9 @@ fn mul(n1: f64, n2: f64) -> f64 {
     return n1 * n2;
 }
 fn div(n1: f64, n2: f64) -> f64 {
-    if n1 == 0.0 {
+    if n2 == 0.0 {
         println!("can't devide by 0");
-        return 0.0;
+        std::process::exit(1);
     } else {
         return n1 / n2;
     }
